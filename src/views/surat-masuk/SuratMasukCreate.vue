f509a250d01864762fc2afaa1651472dde9c4e77
Warso <warso@std.moestopo.ac.id>
2026-07-11 03:02:12 +0700

    Add SuratMasukCreate component with SuratMasukForm

---
+++ src/views/surat-masuk/SuratMasukCreate.vue
@@ -0,0 +1,11 @@
+<template>
+  <SuratMasukForm />
+</template>
+
+<script>
+import SuratMasukForm from './SuratMasukForm.vue'
+export default {
+  name: 'SuratMasukCreate',
+  components: { SuratMasukForm }
+}
+</script>
