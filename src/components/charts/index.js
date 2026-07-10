f482d515ee5d1eb9d69ef63308f8f38013d7aa9f
Warso <warso@std.moestopo.ac.id>
2026-07-11 03:05:09 +0700

    Add chart components aggregator in index.js

---
+++ src/components/charts/index.js
@@ -0,0 +1,5 @@
+// Chart components aggregator
+export { default as StatCard } from './StatCard.vue'
+export { default as BarChart } from './BarChart.vue'
+export { default as LineChart } from './LineChart.vue'
+export { default as PieChart } from './PieChart.vue'
