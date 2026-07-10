8e17778904e6a9c39971e8c49b517c11629e8565
Warso <warso@std.moestopo.ac.id>
2026-07-11 03:03:27 +0700

    Create index.js to aggregate store exports

---
+++ src/stores/index.js
@@ -0,0 +1,7 @@
+// Store aggregator
+export { useAuthStore } from './auth'
+export { useAppStore } from './app'
+export { useThemeStore } from './theme'
+export { useSuratMasukStore } from './suratMasuk'
+export { useSuratKeluarStore } from './suratKeluar'
+export { useAdminStore } from './admin'
