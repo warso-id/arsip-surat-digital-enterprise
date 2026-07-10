abd23f5de89100e912433689a91e854a837b503d
Warso <warso@std.moestopo.ac.id>
2026-07-11 03:04:05 +0700

    Add index.js to aggregate composables exports

---
+++ src/composables/index.js
@@ -0,0 +1,6 @@
+// Composables aggregator
+export { useApi, useSuratMasuk, useSuratKeluar, useUsers } from './useApi'
+export { useAuth } from './useAuth'
+export { useOffline } from './useOffline'
+export { useNotification } from './useNotification'
+export { useFileUpload } from './useFileUpload'
