91cf664189d632b6d631facafa82a766811eef0e
Warso <warso@std.moestopo.ac.id>
2026-07-11 03:06:41 +0700

    Add Surat components aggregator index file

---
+++ src/components/surat/index.js
@@ -0,0 +1,4 @@
+// Surat components aggregator
+export { default as SuratCard } from './SuratCard.vue'
+export { default as SuratTimeline } from './SuratTimeline.vue'
+export { default as QRCodeGenerator } from './QRCodeGenerator.vue'
